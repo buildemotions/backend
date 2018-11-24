d99deffcf76d824178cdfec93916f6abdc12b99e
Miguel <mig.pulgar.info@gmail.com>
2018-11-24 16:48:05 -0300

    Script de inicial de la base de datos.

---
+++ script.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE IF NOT EXISTS usuarios (
+	id int AUTO_INCREMENT,
+    usuario VARCHAR(255) ,
+    pass VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255),
+    PRIMARY KEY (id)
+)
+
+
+
+INSERT INTO koayxadwsn00k6ws.usuarios
+(usuario, pass, nombre)
+VALUES('10-9', 'asdf1234', 'Juan Carlos Gomez');
+
+
+INSERT INTO koayxadwsn00k6ws.usuarios
+(usuario, pass, nombre)
+VALUES('18205685-5', 'asdf1234', 'Sebastian Romero');
+
+
+INSERT INTO koayxadwsn00k6ws.usuarios
+(usuario, pass, nombre)
+VALUES('21202098-2', 'asdf1234', 'Marcos Silva');
+
+
+INSERT INTO koayxadwsn00k6ws.usuarios
+(usuario, pass, nombre)
+VALUES('17624764-9', 'asdf1234', 'Esteban Pavez');
+
+
+INSERT INTO koayxadwsn00k6ws.usuarios
+(usuario, pass, nombre)
+VALUES('12054056-4', 'asdf1234', 'Mauricio Fuentes');
